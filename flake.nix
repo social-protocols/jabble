397141d4325d198407b08ad0dc19a9142ea38c0d
Felix Dietze <felix.dietze@rwth-aachen.de>
2023-11-09 13:35:25 +0700

    add direnv and nix shell

---
+++ flake.nix
@@ -0,0 +1,45 @@
+# inspired by https://ayats.org/blog/nix-rustup/
+
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+
+    # for `flake-utils.lib.eachSystem`
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ ];
+          config.allowUnfree = false;
+        };
+      in
+      {
+        devShells = {
+          default = with pkgs; pkgs.mkShellNoCC {
+            buildInputs = [
+              git
+
+              nodePackages.prisma
+              sqlite-interactive
+
+              flyctl
+
+              # darwin.apple_sdk.frameworks.Security
+            ];
+
+            shellHook = with pkgs; ''
+              export PRISMA_SCHEMA_ENGINE_BINARY="${prisma-engines}/bin/schema-engine"
+              export PRISMA_QUERY_ENGINE_BINARY="${prisma-engines}/bin/query-engine"
+              export PRISMA_QUERY_ENGINE_LIBRARY="${prisma-engines}/lib/libquery_engine.node"
+            '';
+          };
+        };
+      }
+    );
+}
+
+
