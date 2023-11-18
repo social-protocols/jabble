2f0198e8d845b4e3d34e51e45cd0c05313cae615
Jonathan R. Warden <john.warden@gmail.com>
2023-11-18 17:43:38 +0100

    Add type annotations, show top note in .tsx

--- app/routes/posts+/$postId.tsx
+++ app/routes/posts+/$postId.tsx
@@ -1,14 +1,15 @@
 // import { Spacer } from '#app/components/spacer.tsx'
 // import { Icon } from '#app/components/ui/icon.tsx'
-import { type DataFunctionArgs, json } from '@remix-run/node'
+import { json, type DataFunctionArgs } from '@remix-run/node'
 // import { Form, Link, useLoaderData, type MetaFunction } from '@remix-run/react'
-import { useLoaderData } from '@remix-run/react'
-
-import invariant from 'tiny-invariant'
-import { z } from 'zod'
 import { GeneralErrorBoundary } from '#app/components/error-boundary.tsx'
+import { find_top_note } from '#app/probabilities.ts'
 import { prisma } from '#app/utils/db.server.ts'
 import { invariantResponse } from '#app/utils/misc.tsx'
+import { type Post } from '@prisma/client'
+import { useLoaderData } from '@remix-run/react'
+import invariant from 'tiny-invariant'
+import { z } from 'zod'
 
 const postIdSchema = z.coerce.number()
 
@@ -16,26 +17,36 @@ export async function loader({ params }: DataFunctionArgs) {
 	invariant(params.postId, 'Missing postid param')
 	const postId: number = postIdSchema.parse(params.postId)
 
-	const post = await prisma.posts.findFirst({
+	const post: Post | null = await prisma.post.findFirst({
 		select: {
 			id: true,
-			parent_id: true,
+			parentId: true,
 			content: true,
+			authorId: true,
+			created: true,
+			// question_id: true,
 		},
 		where: {
 			id: postId,
 		},
 	})
 
+
 	invariantResponse(post, 'Post not found', { status: 404 })
 
-	return json({ post })
+	const tag = "GLOBAL";
+	const note = await find_top_note(tag, postId)
+
+	return json({ post, note })
 }
 
 export default function Post() {
-	const { post } = useLoaderData<typeof loader>()
+	const { post, note } = useLoaderData<typeof loader>()
 
-	return <div>{post.content}</div>
+	return <div>
+		<div>This is a post: {post.content}</div>
+		<div>Top Note: {note ? note.content : ""}</div>
+	</div>
 }
 
 export function ErrorBoundary() {
