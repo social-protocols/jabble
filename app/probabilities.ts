c3146ddbd9206bcdd83bdc0be42fb3ad154fc6c0
Jonathan R. Warden <john.warden@gmail.com>
2023-11-25 10:40:09 +0100

    Factor Tally and BetaDistribution type out of probabilities.ts and make more generic. Adopt typescript naming conventions for functions

--- app/probabilities.ts
+++ app/probabilities.ts
@@ -1,48 +1,24 @@
 import { prisma } from '#app/utils/db.server.ts';
 import { Result } from "@badrap/result";
 
-import { type CurrentInformedTally, type CurrentTally, type Post, type Tag } from '@prisma/client';
-
-import { getOrInsertTagId } from './tag.ts';
+import { type CumulativeStats, type CurrentInformedTally, type CurrentTally, type Post } from '@prisma/client';
 
 import assert from 'assert';
 
-
-function from_alpha_beta(alpha: number, beta: number): BetaDistribution {
-    return new BetaDistribution(
-        alpha / (alpha + beta),
-        alpha + beta,
-    )
-}
+import { BetaDistribution, type Tally } from '#app/beta-distribution.ts';
+import { getOrInsertTagId } from './tag.ts';
 
 
-class BetaDistribution {
-    constructor(public average: number = 0, public weight: number = 0) {}
+//Import data structures
 
-    update(tally: Tally): BetaDistribution {
-        return new BetaDistribution(
-            bayesian_average(this.average, this.weight, tally),
-            this.weight + tally.total,        
-        )
-    }
-} 
+export const WEIGHT_CONSTANT: number = 2.3
 
-function bayesian_average(prior_average: number, weight: number, tally: Tally): number {
-    return (prior_average * weight + tally.upvotes) / (weight + tally.total)
-}
+export const GLOBAL_PRIOR_PROBABILITY = new BetaDistribution(0.875, WEIGHT_CONSTANT)
+export const GLOBAL_PRIOR_VOTE_RATE = new BetaDistribution(1, WEIGHT_CONSTANT)
 
-const WEIGHT_CONSTANT: number = 2.3
-
-const GLOBAL_PRIOR = new BetaDistribution(0.875, WEIGHT_CONSTANT)
-
-
-export type Tally = {
-    readonly upvotes: number,
-    readonly total: number,
-}
 
 const EMPTY_TALLY = {
-    upvotes: 0,
+    count: 0,
     total: 0,
 }
 
@@ -55,117 +31,80 @@ type InformedTally = {
     forNote: Tally,
 }
 
-
-function to_informed_tally(result: CurrentInformedTally, forNote: Tally): InformedTally {
+function toInformedTally(result: CurrentInformedTally, forNote: Tally): InformedTally {
     return {
         postId: result.postId,
         noteId: result.noteId,
         givenNotShownThisNote: {
-            upvotes: result.upvotesGivenNotShownThisNote,
-            total: result.votesGivenNotShownThisNote,
+            count: result.countGivenNotShownThisNote,
+            total: result.totalGivenNotShownThisNote,
         },
         givenShownThisNote: {
-            upvotes: result.upvotesGivenShownThisNote,
-            total: result.votesGivenShownThisNote,
+            count: result.countGivenShownThisNote,
+            total: result.totalGivenShownThisNote,
         },
         forNote: {
-            upvotes: forNote.upvotes,
+            count: forNote.count,
             total: forNote.total,
         },
     }
 }
 
-export async function get_current_tallies(tagId: number, postId: number, map: Map<number, InformedTally[]>) {
-    const results: CurrentInformedTally[] = await prisma.currentInformedTally.findMany({
-        where: {
-            tagId: tagId,
-            postId: postId,
-        },
-    })
 
-    let tallies = await Promise.all(results.map(async result => {
-        await get_current_tallies(tagId, result.noteId, map)
-        let for_note = (await current_tally(tagId, result.noteId))
-
-        return to_informed_tally(result, for_note)
-    }))
-
-    if (tallies.length > 0) {
-        map.set(postId, tallies)
-    }
-
-    // This recursive function code is an alternative to the above recursive query.
-    // The Typescript code is both simpler than the SQL and avoids using executeRaw statements
-    // Which can not guarantee type safety.
-    //     WITH children AS
-    //     (
-    //         SELECT
-    //           postId
-    //         , noteId
-    //         , votes_given_shown_this_note
-    //         , upvotes_given_shown_this_note
-    //         , votes_given_not_shown_this_note
-    //         , upvotes_given_not_shown_this_note
-    //       FROM current_informed_tally p
-    //       WHERE postId = ${postId}
-    //         UNION ALL
-    //       SELECT 
-    //           p.postId
-    //         , p.noteId
-    //         , p.votes_given_not_shown_this_note
-    //         , p.upvotes_given_not_shown_this_note
-    //         , p.votes_given_not_shown_this_note
-    //         , p.upvotes_given_not_shown_this_note
-    //       FROM children c
-    //       INNER JOIN current_informed_tally p ON p.postId = c.noteId
-    //     )
-    //     SELECT 
-    //         children.*
-    //         , current_tally.votes as votes_for_note 
-    //         , current_tally.upvotes as upvotes_for_note
-    //     FROM children join current_tally on(current_tally.postId = children.noteId);
-
-    return
-}
-
-
-
-export async function find_top_note(tag: string, postId: number): Promise<Post | null> {
+export async function voteRate(tag: string, postId: number): Promise<number> {
 
     let tagId = await getOrInsertTagId(tag)
 
+    let tally = await currentTally(tagId, postId)
+    let attention = await cumulativeAttention(tagId, postId)
+
+    console.log(`Cumulative attention for ${postId}, ${attention}`, tally)
+
+    return GLOBAL_PRIOR_VOTE_RATE.update({ count: tally.total, total: attention }).average
+
+}
+
+
+export async function informedProbability(tag: string, postId: number): Promise<number> {
+
+    let tagId = await getOrInsertTagId(tag)
+
+    const [_noteId, _p, q] = await findTopNoteId(tagId, postId);
+
+    return q
+}
+
+
+export async function topNote(tag: string, postId: number): Promise<Post | null> {
+
+    let tagId = await getOrInsertTagId(tag)
+
+    const [noteId, p, q] = await findTopNoteId(tagId, postId);
+
+    if (noteId == 0) {
+        return null
+    }
+
+    return await prisma.post.findUniqueOrThrow({ where: { id: noteId } })
+}
+
+
+async function findTopNoteId(tagId: number, postId: number): Promise<[number, number, number]> {
+
     let talliesMap = new Map<number, InformedTally[]>()
-    await get_current_tallies(tagId, postId, talliesMap)
+    await getCurrentTallies(tagId, postId, talliesMap)
 
     console.log("Current tallies", talliesMap)
 
-    let tally = await current_tally(tagId, postId)
+    let tally = await currentTally(tagId, postId)
 
-    const [noteId, p, q] = find_top_note_given_tallies(postId, tally, talliesMap);
-
-    console.log("Top note", noteId, p, q)
-
-    let note: Post = await prisma.post.findUniqueOrThrow({ where: { id: noteId } })
-
-    return note;
-} 
-
-
-async function current_tally(tagId: number, postId: number): Promise<Tally> {
-
-    const tally: CurrentTally | null = await prisma.currentTally.findFirst({
-        where: {
-            tagId: tagId,
-            postId: postId,
-        },
-    })
-
-    if (tally == null) {
-        return EMPTY_TALLY
+    let result = await findTopNoteGivenTallies(postId, tally, talliesMap);
+    if (result == null) {
+        return [0, 0, 0]
     }
+    return result
 
-    return tally;
-}
+} 
 
 
 // In the context of this function, we always have two posts in scope: A post along with a
@@ -173,12 +112,12 @@ async function current_tally(tagId: number, postId: number): Promise<Tally> {
 // and function names).
 // The function recurses through the tree of a conversation started by the post `postId`,
 // always looking at post/note combinations.
-export function find_top_note_given_tallies(
+export function findTopNoteGivenTallies(
     postId: number,
     postTally: Tally,
     subnoteTallies: Map<number, InformedTally[]>,
 ): [number, number, number] {
-    let pOfAGivenNotShownTopNote = GLOBAL_PRIOR.update(postTally).average;
+    let pOfAGivenNotShownTopNote = GLOBAL_PRIOR_PROBABILITY.update(postTally).average;
 
     let pOfAGivenShownTopNote = pOfAGivenNotShownTopNote;
 
@@ -202,13 +141,13 @@ export function find_top_note_given_tallies(
     for (let tally of tallies) {
 
         let [_, p_of_b_given_shown_top_subnote, pOfBGivenNotShownTopSubnote] =
-            find_top_note_given_tallies(tally.noteId, tally.forNote, subnoteTallies);
+            findTopNoteGivenTallies(tally.noteId, tally.forNote, subnoteTallies);
         let support = p_of_b_given_shown_top_subnote / pOfBGivenNotShownTopSubnote;
 
-        let pOfAGivenNotShownThisNote = GLOBAL_PRIOR
+        let pOfAGivenNotShownThisNote = GLOBAL_PRIOR_PROBABILITY
             .update(tally.givenNotShownThisNote)
             .average;
-        let pOfAGivenShownThisNote = GLOBAL_PRIOR
+        let pOfAGivenShownThisNote = GLOBAL_PRIOR_PROBABILITY
             .update(tally.givenNotShownThisNote)
             .update(tally.givenShownThisNote)
             .average;
@@ -235,3 +174,92 @@ export function find_top_note_given_tallies(
         pOfAGivenNotShownTopNote,
     ]
 }
+
+
+
+async function currentTally(tagId: number, postId: number): Promise<Tally> {
+
+    const tally: CurrentTally | null = await prisma.currentTally.findFirst({
+        where: {
+            tagId: tagId,
+            postId: postId,
+        },
+    })
+
+    if (tally == null) {
+        return EMPTY_TALLY
+    }
+
+    return tally;
+}
+
+
+async function cumulativeAttention(tagId: number, postId: number): Promise<number> {
+
+    const stats: CumulativeStats | null = await prisma.cumulativeStats.findFirst({
+        where: {
+            tagId: tagId,
+            postId: postId,
+        },
+    })
+
+    if (stats == null) {
+        return 0
+    }
+
+    return stats.attention;
+}
+
+
+
+export async function getCurrentTallies(tagId: number, postId: number, map: Map<number, InformedTally[]>) {
+    const results: CurrentInformedTally[] = await prisma.currentInformedTally.findMany({
+        where: {
+            tagId: tagId,
+            postId: postId,
+        },
+    })
+
+    let tallies = await Promise.all(results.map(async result => {
+        await getCurrentTallies(tagId, result.noteId, map)
+        let for_note = (await currentTally(tagId, result.noteId))
+
+        return toInformedTally(result, for_note)
+    }))
+
+    if (tallies.length > 0) {
+        map.set(postId, tallies)
+    }
+
+    // This recursive function code is an alternative to the above recursive query.
+    // The Typescript code is both simpler than the SQL and avoids using executeRaw statements
+    // Which can not guarantee type safety.
+    //     WITH children AS
+    //     (
+    //         SELECT
+    //           postId
+    //         , noteId
+    //         , votes_given_shown_this_note
+    //         , upvotes_given_shown_this_note
+    //         , votes_given_not_shown_this_note
+    //         , upvotes_given_not_shown_this_note
+    //       FROM current_informed_tally p
+    //       WHERE postId = ${postId}
+    //         UNION ALL
+    //       SELECT 
+    //           p.postId
+    //         , p.noteId
+    //         , p.count_given_not_shown_this_note
+    //         , p.count_given_not_shown_this_note
+    //       FROM children c
+    //       INNER JOIN current_informed_tally p ON p.postId = c.noteId
+    //     )
+    //     SELECT 
+    //         children.*
+    //         , currentTally    //         , currentTally.count as upvotes_for_note
+    //     FROM children join currentTally on(currentTally.postId = children.noteId);
+
+    return
+}
+
+
--- app/routes/posts+/$postId.tsx
+++ app/routes/posts+/$postId.tsx
@@ -4,7 +4,7 @@ import { json, type DataFunctionArgs } from '@remix-run/node'
 // import { Form, Link, useLoaderData, type MetaFunction } from '@remix-run/react'
 import { Location, logImpression } from "#app/attention.ts"
 import { GeneralErrorBoundary } from '#app/components/error-boundary.tsx'
-import { find_top_note } from '#app/probabilities.ts'
+import { topNote, voteRate } from '#app/probabilities.ts'
 import { prisma } from '#app/utils/db.server.ts'
 import { invariantResponse } from '#app/utils/misc.tsx'
 import { type Post } from '@prisma/client'
@@ -40,12 +40,16 @@ export async function loader({ params }: DataFunctionArgs) {
 	invariantResponse(post, 'Post not found', { status: 404 })
 
 	const tag = GLOBAL_TAG;
-	const note = await find_top_note(tag, postId)
+	const note = await topNote(tag, postId)
+
+	const vr = await voteRate(tag, postId)
+	console.log(`Vote rate is ${vr}`)
+
 
 	const noteId = note != null ? note.id : 0
 
-	await logImpression(tag, postId, Location.POST_PAGE, 0)
-	await logImpression(tag, noteId, Location.TOP_NOTE_ON_POST_PAGE, 0)
+	await logImpression(100, tag, postId, Location.POST_PAGE, 0)
+	await logImpression(100, tag, noteId, Location.TOP_NOTE_ON_POST_PAGE, 0)
 
 	return json({ post, note })
 }
