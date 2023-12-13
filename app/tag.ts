f90fa9e70831e18a9ef87b6b40f1147eed96e082
Jonathan R. Warden <john.warden@gmail.com>
2023-12-13 15:18:47 +0100

    Use transaction in getOrInsertTagId

--- app/tag.ts
+++ app/tag.ts
@@ -1,5 +1,4 @@
 import assert from 'assert'
-import { sql, type Selectable } from 'kysely'
 import { type Tag } from '#app/db/types.ts' // this is the Database interface we defined earlier
 import { db } from '#app/db.ts'
 
@@ -7,17 +6,25 @@ export async function getOrInsertTagId(tag: string): Promise<number> {
 	// let t: Selectable<Tag>[] = await db.selectFrom("tag").where("tag", "=", tag).selectAll().execute()
 
 	// return 0
+	const t = await db.transaction().execute(async trx => {
+		let t: Tag[] = await trx
+			.selectFrom('Tag')
+			.where('tag', '=', tag)
+			.selectAll()
+			.execute()
 
-	let t: Selectable<Tag>[] = (
-		await sql<Selectable<Tag>>`select * from tag where tag = ${tag}`.execute(db)
-	).rows
+		if (t.length == 0) {
+			// use drizzle to create new tag
+			let _newTag = await trx.insertInto('Tag').values({ tag: tag }).execute()
+		}
 
-	if (t.length == 0) {
-		// use drizzle to create new tag
-		let _newTag = await db.insertInto('Tag').values({ tag: tag }).execute()
-	}
+		return await trx
+			.selectFrom('Tag')
+			.where('tag', '=', tag)
+			.selectAll()
+			.execute()
+	})
 
-	t = await db.selectFrom('Tag').where('tag', '=', tag).selectAll().execute()
-	assert(t.length == 1)
+	assert(t[0] !== undefined)
 	return t[0]!.id
 }
