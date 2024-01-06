bea5feb8e22a48ca5c80228938558eaa817f1f13
Felix Dietze <felix.dietze@rwth-aachen.de>
2024-01-06 21:40:28 +1100

    fix wording

--- app/routes/settings+/profile.change-email.tsx
+++ app/routes/settings+/profile.change-email.tsx
@@ -70,7 +70,7 @@ export async function handleVerification({
 
 	void sendEmail({
 		to: preUpdateUser.email,
-		subject: 'Epic Stack email changed',
+		subject: 'Email changed',
 		react: <EmailChangeNoticeEmail userId={user.id} />,
 	})
 
