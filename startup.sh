ecea81afadc549c389ffdc6b71e9ab601ea0babf
Jonathan R. Warden <john.warden@gmail.com>
2024-04-13 17:27:38 +0200

    Reset global brain DB on startup

--- startup.sh
+++ startup.sh
@@ -6,5 +6,11 @@ npm run migrate
 npm start &
 cd GlobalBrain.jl-0.1
 touch "$VOTE_EVENTS_PATH"
+
+# Reinit global brain database and reprocess all vote events.
+rm -f "$SCORE_EVENTS_PATH"
+rm -f "$GB_DATABASE_PATH"
+
 touch "$SCORE_EVENTS_PATH"
+
 tail -n +0 -F "$VOTE_EVENTS_PATH" | /opt/julia-1.9.4/bin/julia --project scripts/run.jl "$GB_DATABASE_PATH" - "$SCORE_EVENTS_PATH"
