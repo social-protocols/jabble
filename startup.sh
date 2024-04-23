7fdf32019e37804d98f43fea3ed78e36a4fe67c6
Jonathan R. Warden <john.warden@gmail.com>
2024-04-23 12:51:09 +0200

    Add GLOBALBRAIN_VERSION to environment

--- startup.sh
+++ startup.sh
@@ -16,5 +16,5 @@ rm -f "$GB_DATABASE_PATH"
 
 npm start &
 
-cd GlobalBrain.jl-0.1.1
+cd GlobalBrain.jl-$GLOBALBRAIN_VERSION
 tail -n +0 -F "$VOTE_EVENTS_PATH" | /opt/julia-1.9.4/bin/julia --project scripts/run.jl "$GB_DATABASE_PATH" - "$SCORE_EVENTS_PATH"
