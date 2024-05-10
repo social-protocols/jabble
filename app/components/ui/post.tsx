a202a2ff028457b59958c1e6e54ad19d90163d95
Johannes Nakayama <johannes.nakayama@rwth-aachen.de>
2024-05-10 09:48:52 +0200

    Remove note attachment

--- app/components/ui/post.tsx
+++ app/components/ui/post.tsx
@@ -5,16 +5,13 @@ import { Markdown } from '#app/components/markdown.tsx'
 import { Textarea } from '#app/components/ui/textarea.tsx'
 import { type Post } from '#app/db/types.ts'
 import { type ScoredPost, type ScoredNote } from '#app/ranking.ts'
-import { relativeEntropy } from '#app/utils/entropy.ts'
 import { Direction, type VoteState } from '#app/vote.ts'
-import { Card } from './card.tsx'
 import { Truncate } from './Truncate.tsx'
 
 /* Keep this relatively high, so people don't often have to click "read more"
    to read most content. But also not too high, so people don't have to
    scroll past big walls of text of posts they are not interested in */
 const postTeaserMaxLines = 20
-const noteMaxLines = 20
 
 export function PostContent({
 	content,
@@ -143,7 +140,6 @@ export function PostDetails({
 					deactivateLinks={false}
 					linkTo={`/tags/${post.tag}/posts/${post.id}`}
 				/>
-				{note && <NoteAttachment note={note} tag={post.tag} className="mt-2" />}
 
 				<div className="mt-2 flex w-full text-sm">
 					<Link to={`/tags/${post.tag}/posts/${post.id}`} className="ml-2">
@@ -239,45 +235,6 @@ function ReplyForm({
 				</button>
 			</div>
 		</div>
-	)
-}
-
-export function NoteAttachment({
-	tag,
-	note,
-	className,
-}: {
-	note: ScoredNote
-	tag: string
-	className: string
-}) {
-	// TODO: what is a reasonable relativeEntropy cutoff? The algorithm now, using the simplified Bayesian algorithm calculation (to avoid MCMC or variational methods)
-	// results in a different p and q even if there are no informed votes. So there will always be some relative entropy...
-
-	const effectString =
-		relativeEntropy(note.p, note.q) < 0.01
-			? ''
-			: (note.p > note.q ? '↑' : '↓') +
-				Math.abs(Math.round((note.p - note.q) * 100)) +
-				'%'
-	return (
-		<Link to={`/tags/${tag}/posts/${note.id}`}>
-			<Card className={'bg-note pb-3 pt-2 text-note-foreground ' + className}>
-				<div className="pb-1 text-sm font-medium opacity-50">
-					Featured Reply <span>{effectString}</span>
-					{/*<a href="/faq#noteEffect"> ⓘ</a>*/}
-					{/*<span>Debug info: {relativeEntropy(note.p, note.q)} / {note.p} / {note.q} / {note.pCount}/{note.pSize} {note.qCount}/{note.qSize}</span>*/}
-				</div>
-
-				{note && (
-					<PostContent
-						content={note.content}
-						maxLines={noteMaxLines}
-						deactivateLinks={true}
-					/>
-				)}
-			</Card>
-		</Link>
 	)
 }
 
