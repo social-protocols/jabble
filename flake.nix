1542f3351db3a81311ca24135d9dbc49a1266d72
Felix Dietze <github@felx.me>
2024-05-26 11:50:18 +0200

    update flake

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
 
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/ba733f8000925e837e30765f273fec153426403d";
+    nixpkgs.url = "github:nixos/nixpkgs";
 
     # for `flake-utils.lib.eachSystem`
     flake-utils.url = "github:numtide/flake-utils";
