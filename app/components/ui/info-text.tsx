8a56998745b97dc954ab3eaa57220a4f51c2e5af
Felix Dietze <felix.dietze@rwth-aachen.de>
2024-07-06 13:26:55 +0100

    update header text (#177)

--- app/components/ui/info-text.tsx
+++ app/components/ui/info-text.tsx
@@ -4,13 +4,13 @@ export function InfoText({ className }: { className?: string }) {
 	const infoText = `
 ### Hey, you are part of our early experiments! 🚀
 
-This platform ranks content not only by how many votes it received, but
-according to **how convincing** it is. The more you vote, the more accurate the convincingness score becomes
-([learn more](https://github.com/social-protocols/social-network#readme)).
+Jabble is designed to make conversations on the Internet more intelligent and less polarized. 
+It does this by ranking content not only by how many votes it received, but according to **how convincing** (🔥) it is. 
+The more you comment and vote in the conversation under a post, the more influence your votes will have ([learn more](https://github.com/social-protocols/social-network#readme)).
+
 Everything you see here is work in progress.
 We would love to hear your feedback!
-
-Use [this feedback form](https://forms.gle/dzJtsTJwPSsBihPUA) or submit a [GitHub issue](https://github.com/social-protocols/jabble/issues).
+Please use [this feedback form](https://forms.gle/dzJtsTJwPSsBihPUA) or submit a [GitHub issue](https://github.com/social-protocols/jabble/issues).
 	`
 
 	return (
