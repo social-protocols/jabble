1c72bcf53144a65c5f360d2344913490e770b632
Felix Dietze <github@felx.me>
2024-09-01 15:32:22 +0200

    fix replay script

--- other/replay.sh
+++ other/replay.sh
@@ -2,6 +2,8 @@
 # https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail
 set -Eeuo pipefail
 
+# cd to script folder
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
 rm -f $GB_DATABASE_PATH
 npx tsx replay-vote-events.ts
-
