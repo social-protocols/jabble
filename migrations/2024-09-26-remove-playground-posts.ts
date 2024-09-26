547743f87d86721e55a0c335b9b27d9db1378a06
Johannes Nakayama <johannes.nakayama@rwth-aachen.de>
2024-09-26 21:21:51 +0200

    remove playground posts for fallacy detection

---
+++ migrations/2024-09-26-remove-playground-posts.ts
@@ -0,0 +1,9 @@
+import { type Kysely, sql } from 'kysely'
+
+export async function up(db: Kysely<any>): Promise<void> {
+	await db.transaction().execute(async trx => {
+		await sql`
+			drop table PlaygroundPost
+		`.execute(trx)
+	})
+}
