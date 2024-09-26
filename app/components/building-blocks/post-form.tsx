d658b15881b719732cbc93d7ddae20f950684b6f
Johannes Nakayama <johannes.nakayama@rwth-aachen.de>
2024-09-26 16:52:39 +0200

    add spinning loader to discussion submission

--- app/components/building-blocks/post-form.tsx
+++ app/components/building-blocks/post-form.tsx
@@ -1,5 +1,6 @@
 import { useFetcher } from '@remix-run/react'
 import { type ChangeEvent, useState, type FormEvent } from 'react'
+import { Icon } from '#app/components/ui/icon.tsx'
 import { Textarea } from '#app/components/ui/textarea.tsx'
 import { MAX_CHARS_PER_POST } from '#app/constants.ts'
 
@@ -52,7 +53,14 @@ export function PostForm({ className }: { className?: string }) {
 						disabled={replyFetcher.state !== 'idle'}
 						className="rounded bg-blue-200 px-4 py-2 text-base font-bold text-black hover:bg-blue-300"
 					>
-						{replyFetcher.state === 'idle' ? 'Discuss' : 'Submitting...'}
+						{replyFetcher.state === 'idle' ? (
+							<>Discuss</>
+						) : (
+							<>
+								Submitting
+								<Icon name="update" className="ml-2 animate-spin" />
+							</>
+						)}
 					</button>
 				</div>
 			</div>
