1613f8ba303ece639e57477109f8ab1a2f5848df
Johannes Nakayama <johannes.nakayama@rwth-aachen.de>
2024-10-01 09:59:16 +0200

    fix claim simplifaciotn migration

--- migrations/2024-09-27-simplify-claim-model.ts
+++ migrations/2024-09-27-simplify-claim-model.ts
@@ -8,11 +8,6 @@ export async function up(db: Kysely<any>): Promise<void> {
 		`.execute(trx)
 
 		await sql`
-			alter table Poll
-			rename to Poll__DEP
-		`.execute(trx)
-
-		await sql`
 			create table Claim (
 				  id         integer not null primary key autoincrement
 				, quoteId    integer          references Quote(id) default null
@@ -24,33 +19,59 @@ export async function up(db: Kysely<any>): Promise<void> {
 
 		await sql`
 			insert into Claim (
-				id
-				, quoteId
+				  id
 				, claim
-				, postId
-				, createdAt
 			)
 			select
-				id
-				, quoteId
+				  id
 				, claim
-				, postId
-				, createdAt
-			from CandidateClaim
+			from Claim__DEP
+		`.execute(trx)
+
+		await sql`
+			with ClaimToPost as (
+				select
+					claimId as id
+					, postId
+				from Poll
+			)
+			update Claim
+			set postId = ctp.postId
+			from ClaimToPost ctp
+			where Claim.id = ctp.id
+		`.execute(trx)
+
+		await sql`
+			with ClaimToQuote as (
+				select
+					  quoteId
+					, claim
+				from CandidateClaim
+				where postId is not null
+			)
+			update Claim
+			set quoteId = ctq.quoteId
+			from ClaimToQuote ctq
+			where Claim.claim = ctq.claim
+		`.execute(trx)
+
+		await sql`
+			alter table Poll
+			rename to Poll__DEP
 		`.execute(trx)
 
 		await sql`
 			create table Poll (
-				claimId integer not null references Claim(id)
-				, postId integer not null references Post(id)
-				, pollType text not null
+				  claimId  integer not null references Claim(id)
+				, postId   integer not null references Post(id)
+				, pollType text    not null
 				, primary key (claimId)
 			)
 		`.execute(trx)
 
 		await sql`
 			insert into Poll (
-				claimId
+				  claimId
 				, postId
 				, pollType
 			)
